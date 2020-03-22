69656740e1a8510a04506308f6e94ffe07abad17
Jeongwook Park <henrysecond1@gmail.com>
2020-03-22 09:28:44 +0900

    Add practice for 05-kubernetes-configuration-files

---
+++ practice/05/generate_kube_configs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kube-the-hard-way \
+  --region asia-northeast1 \
+  --format 'value(address)')
+
+for instance in worker-0 worker-1 worker-2; do
+  kubectl config set-cluster kube-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config set-credentials system:node:${instance} \
+    --client-certificate=${instance}.pem \
+    --client-key=${instance}-key.pem \
+    --embed-certs=true \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:node:${instance} \
+    --kubeconfig=${instance}.kubeconfig
+
+  kubectl config use-context default --kubeconfig=${instance}.kubeconfig
+done
